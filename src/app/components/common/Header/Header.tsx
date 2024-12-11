8e4ee9b8ad0207dbd83a37bca109945e6803fc3c
Daniel San <danielgdt88@hotmail.com>
2024-12-11 19:52:03 -0300

    feat: adiciono font poppins no header

--- src/app/components/common/Header/Header.tsx
+++ src/app/components/common/Header/Header.tsx
@@ -2,7 +2,7 @@ import MenuHamburguer from './MenuHamburguer';
 
 export default function Header() {
   return (
-    <header className='flex justify-between bg-brand-brown text-primary-white h-[72px] items-center px-6 text-[20px]'>
+    <header className='flex font-poppins justify-between bg-brand-brown text-primary-white h-[72px] items-center px-6 text-[20px]'>
       <div>
         Missão <span className='font-bold'>Itapicuru</span>
       </div>
