c0be548495f28ba3b1afc3668477f9d37c95a621
Daniel San <danielgdt88@hotmail.com>
2024-12-11 19:49:53 -0300

    feat: componentizo menu hamburguer

--- src/app/components/common/Header/Header.tsx
+++ src/app/components/common/Header/Header.tsx
@@ -1,14 +1,12 @@
+import MenuHamburguer from './MenuHamburguer';
+
 export default function Header() {
   return (
     <header className='flex justify-between bg-brand-brown text-primary-white h-[72px] items-center px-6 text-[20px]'>
       <div>
         Missão <span className='font-bold'>Itapicuru</span>
       </div>
-      <div className='flex flex-col gap-y-2'>
-        <span className='menu-hamburguer'></span>
-        <span className='menu-hamburguer'></span>
-        <span className='menu-hamburguer-last'></span>
-      </div>
+      <MenuHamburguer />
     </header>
   );
 }
---
+++ src/app/components/common/Header/MenuHamburguer.tsx
@@ -0,0 +1,9 @@
+export default function MenuHamburguer() {
+  return (
+    <div className='flex flex-col gap-y-2 cursor-pointer'>
+      <span className='menu-hamburguer'></span>
+      <span className='menu-hamburguer'></span>
+      <span className='menu-hamburguer-last'></span>
+    </div>
+  );
+}
