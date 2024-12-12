f7fd49d03386fb5c20192dd3e534d5dc034f5386
Daniel San <danielgdt88@hotmail.com>
2024-12-12 19:43:45 -0300

    feat: finalizo componente de menu hamburguer

--- src/components/common/Menu-Hamburguer/MenuHamburguer.tsx
+++ src/components/common/Menu-Hamburguer/MenuHamburguer.tsx
@@ -1,8 +1,12 @@
+import FacebookIcon from '../../../../public/svg/facebookIcon';
+import InstagramIcon from '../../../../public/svg/instagramIcon';
+import YoutubeIcon from '../../../../public/svg/youtubeIcon';
+
 export default function MenuHamburguer({ menuOpen }: { menuOpen: boolean }) {
   if (!menuOpen) return null;
   return (
-    <div className='pt-[72px] h-full bg-brand-brown flex flex-col items-center'>
-      <ul className='flex flex-col text-[24px] text-center text-primary-white font-dm-sans font-bold gap-y-[48px] pt-14'>
+    <div className='pt-[72px] h-full bg-brand-brown flex flex-col items-center font-dm-sans'>
+      <ul className='flex flex-col text-[24px] text-center text-primary-white font-bold gap-y-[48px] pt-14'>
         <li>Início</li>
         <li>Missão</li>
         <li>Valores</li>
@@ -10,6 +14,16 @@ export default function MenuHamburguer({ menuOpen }: { menuOpen: boolean }) {
         <li>Onde servimos</li>
         <li>Declaração de fé</li>
       </ul>
+
+      <button className='rounded-[40px] bg-white text-brand-brown text-[18px] font-bold mt-[43px] mb-[74px] w-[170px] h-[55px]'>
+        DOE AGORA
+      </button>
+
+      <div className='flex gap-x-8 mb-[176px]'>
+        <InstagramIcon />
+        <FacebookIcon />
+        <YoutubeIcon />
+      </div>
     </div>
   );
 }
