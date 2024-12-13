9f586c21812098475ee9072a95c5107c0db791e0
Daniel San <danielgdt88@hotmail.com>
2024-12-13 17:54:16 -0300

    feat: crio componente home e finalizo título

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className='pt-[72px] bg-brand-light font-dm-sans text-center'>
+      <h1 className='font-bold pt-10 pb-8 text-brand-brown text-[14px]'>
+        BOAS-VINDAS A MISSÃO ITAPICURU
+      </h1>
+    </div>
+  );
+}
