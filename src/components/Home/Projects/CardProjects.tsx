301ddadcfe9225fcab4ad95a415712232ba36236
Daniel San <danielgdt88@hotmail.com>
2024-12-15 20:57:12 -0300

    feat: crio componente do title e card para projects da home

---
+++ src/components/Home/Projects/CardProjects.tsx
@@ -0,0 +1,3 @@
+export default function CardProjects() {
+  return <div>card</div>;
+}
--- src/components/Home/ProjectsHome.tsx
+++ src/components/Home/ProjectsHome.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image';
+import TitleProjects from './Projects/TitleProjects';
+import CardProjects from './Projects/CardProjects';
 
 export default function ProjectsHome() {
   return (
@@ -15,18 +17,8 @@ export default function ProjectsHome() {
         src={'/assets/chapeuHome.png'}
         alt='chapeu de cangaceiro'
       />
-      <div className='flex flex-col text-brand-brown text-center text-[25px] leading-[31px] pb-16'>
-        <h1 className='font-dm-sans font-bold text-sm pb-[19px]'>
-          NOSSOS PROJETOS
-        </h1>
-        <h2 className='font-montserrat px-[30px] font-medium'>
-          Através desses projetos,{' '}
-          <span className='font-bold'>ajudamos a espalhar a mensagem</span>{' '}
-          <p>
-            <span className='font-bold pr-2'>de Deus</span>por onde vamos.
-          </p>
-        </h2>
-      </div>
+      <TitleProjects />
+      <CardProjects />
     </section>
   );
 }
