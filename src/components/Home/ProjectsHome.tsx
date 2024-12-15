6f0556ed75d53c2c76cabac42ee5a8e342b7ba55
Daniel San <danielgdt88@hotmail.com>
2024-12-15 20:52:40 -0300

    feat: crio texto dos projetos

--- src/components/Home/ProjectsHome.tsx
+++ src/components/Home/ProjectsHome.tsx
@@ -15,6 +15,18 @@ export default function ProjectsHome() {
         src={'/assets/chapeuHome.png'}
         alt='chapeu de cangaceiro'
       />
+      <div className='flex flex-col text-brand-brown text-center text-[25px] leading-[31px] pb-16'>
+        <h1 className='font-dm-sans font-bold text-sm pb-[19px]'>
+          NOSSOS PROJETOS
+        </h1>
+        <h2 className='font-montserrat px-[30px] font-medium'>
+          Através desses projetos,{' '}
+          <span className='font-bold'>ajudamos a espalhar a mensagem</span>{' '}
+          <p>
+            <span className='font-bold pr-2'>de Deus</span>por onde vamos.
+          </p>
+        </h2>
+      </div>
     </section>
   );
 }
