1777e2c7033a77ac353d9b084241d8bf5e5ebd6f
Daniel San <danielgdt88@hotmail.com>
2024-12-14 22:41:50 -0300

    feat: alinho imagem da criança

---
+++ src/components/Home/ProjectsHome.tsx
@@ -0,0 +1,11 @@
+export default function ProjectsHome() {
+  return (
+    <section className='relative'>
+      <div className='bg-criancaHome bg-cover w-[327px] h-[470px] mb-12 font-black font-poppins text-4xl text-white text-start  flex rounded-md textShadow absolute -translate-y-2/4 left-6'>
+        <span className='self-end pr-[31px] pl-[20px] pb-[10px]'>
+          Mudamos para sempre o jeito de ver o mundo.
+        </span>
+      </div>
+    </section>
+  );
+}
