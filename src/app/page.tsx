c0a62260bba5225081b6f46ac0b42b8625a3ab1c
Daniel San <danielgdt88@hotmail.com>
2024-12-16 19:27:47 -0300

    feat: crio texto do sobre nós

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Home from '@/components/Home/Home';
 import ProjectsHome from '@/components/Home/ProjectsHome';
+import SobreNos from '@/components/Home/SobreNos';
 
 export default function PageHome() {
   return (
     <main>
       <Home />
       <ProjectsHome />
+      <SobreNos />
     </main>
   );
 }
---
+++ src/components/Home/SobreNos.tsx
@@ -0,0 +1,39 @@
+export default function SobreNos() {
+  return (
+    <section className='bg-brand-beige'>
+      <h1 className='font-bold font-dm-sans text-[14px] text-brand-brown pt-[100px] pl-6'>
+        SOBRE NÓS
+      </h1>
+      <h2 className='font-bold font-dm-sans text-brand-brown text-[30px] pt-4 px-6 pb-6'>
+        Entenda quem somos e por que existimos
+      </h2>
+      <div className='flex flex-col px-6 gap-y-4 text-[11px] font-montserrat font-medium pb-[32px]'>
+        <p>
+          A Missão Casa Farol Sertão Missão Itapicuru é uma organização
+          missionária comprometida em levar a boa notícia para os povos não
+          alcançados do sertão, aqueles que não ouviram nada ou muito pouco
+          sobre Cristo.No ano de 2010 o pastor Savastane após uma busca no site
+          do IBGE, descobriu que o município de Itapicuru possuía o menor IDH da
+          Bahia. A partir de então, passou a visitar a cidade com uma caravana
+          de jovens para evangelizar a cidade e atender de forma emergencial
+          algumas famílias através da distribuição de cestas básicas. 
+        </p>
+        <p>
+          Em 2014 o pastor Josival Amorim junto com o amigo Carlos Eduardo
+          (Cadu), tentaram iniciar um projeto de evangelização nas praças de
+          Itapicuru, mas após uma reunião com as lideranças da cidade chegaram a
+          conclusão de que o Senhor desejava “resgatar uma noiva” que estava
+          ferida. Iniciou-se então um trabalho de discipulado e aconselhamento
+          na Primeira Igreja Batista de Itapicuru. 
+        </p>
+        <p>
+          Em 2015, o pastor Wagner Ribeiro passou a integrar a equipe com
+          visitas frequentes à cidade aos finais de semana, trabalhando com
+          discipulado, fundamentos da fé cristã nas EBD, seminários de dons e
+          Escola de Capacitação Ministerial através da Escola Siga-me (na época
+          Ceifar).
+        </p>
+      </div>
+    </section>
+  );
+}
