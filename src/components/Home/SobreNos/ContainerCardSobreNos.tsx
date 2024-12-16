8b0a101e656348d72513c71ee57d612a2eccf2fe
Daniel San <danielgdt88@hotmail.com>
2024-12-16 20:04:39 -0300

    feat: finalizo container do card sobre nós

--- src/components/Home/SobreNos/ContainerCardSobreNos.tsx
+++ src/components/Home/SobreNos/ContainerCardSobreNos.tsx
@@ -2,14 +2,14 @@ import CardSobreNos from './CardSobreNos';
 
 export default function ContainerCardSobreNos() {
   return (
-    <div className='flex flex-col px-[35px] gap-y-11 pb-24'>
+    <div className='flex flex-col px-[35px] gap-y-11 pb-24 font-poppins font-black text-white'>
       <CardSobreNos bgImage='cardOneSobreNos'>
-        <span className='inline-block font-poppins font-black text-[40px] leading-[39px] text-white pr-[41px] pl-5 pt-[118px]'>
+        <span className='inline-block text-[40px] leading-[39px] pr-[41px] pl-5 pt-[118px]'>
           MISSÃO, VALOR E VISÃO
         </span>
       </CardSobreNos>
       <CardSobreNos bgImage='cardTwoSobreNos'>
-        <div className='font-poppins font-black text-[20px] leading-[19px] text-white pl-[30px] pt-[185px]'>
+        <div className='text-[20px] leading-[19px] pl-[30px] pt-[185px]'>
           <span className='inline-block '>Oque fazemos?</span>
           <p className='text-xs leading-[12px]'>
             Trabalhamos com a plantação e revitalização de Igrejas em povos não
@@ -19,7 +19,19 @@ export default function ContainerCardSobreNos() {
           </p>
         </div>
       </CardSobreNos>
-      <CardSobreNos bgImage='cardThreeSobreNos'>a</CardSobreNos>
+      <CardSobreNos bgImage='cardThreeSobreNos'>
+        <div className='pl-3 pt-24'>
+          <h1 className='text-4xl'>É a sua vez de contribuir.</h1>
+          <p className='text-[10px] leading-[10px] pr-12 pt-4'>
+            Queremos compartilhar a nossa missão no Brasil com cada um de
+            vocês. Através da nossa visão, você pode fazer parte dessa corrente
+            e iniciar uma história. Nós ajudamos pessoas com sua ajuda.
+          </p>
+        </div>
+        <button className='font-dm-sans font-bold text-[10px] leading-[13px] w-[78px] h-[25px] rounded-[40px] bg-brand-light ml-[18px] mt-[23px] cursor-pointer'>
+          SAIBA MAIS
+        </button>
+      </CardSobreNos>
     </div>
   );
 }
