b35c2f0c50085e26db29215dd43524536df90a61
Daniel San <danielgdt88@hotmail.com>
2024-12-15 21:04:51 -0300

    feat: crio svg pro checked e adiciono no card

--- src/components/Home/Projects/CardProjects.tsx
+++ src/components/Home/Projects/CardProjects.tsx
@@ -1,3 +1,11 @@
+import CheckedIcon from '../../../../public/svg/CheckedIcon';
+
 export default function CardProjects() {
-  return <div>card</div>;
+  return (
+    <div className='bg-white w-[248px] h-[238px] border border-brand-light rounded-[30px]'>
+      <span className='ml-6 mt-[9px] inline-block bg-brand-light rounded-full p-1'>
+        <CheckedIcon />
+      </span>
+    </div>
+  );
 }
--- src/components/Home/ProjectsHome.tsx
+++ src/components/Home/ProjectsHome.tsx
@@ -4,14 +4,14 @@ import CardProjects from './Projects/CardProjects';
 
 export default function ProjectsHome() {
   return (
-    <section className='relative'>
+    <section className='relative flex flex-col items-center'>
       <div className='bg-criancaHome bg-cover w-[327px] h-[470px] mb-12 font-black font-poppins text-4xl text-white text-start  flex rounded-md textShadow absolute -translate-y-2/4 left-6'>
         <span className='self-end pr-[31px] pl-[20px] pb-[10px]'>
           Mudamos para sempre o jeito de ver o mundo.
         </span>
       </div>
       <Image
-        className='pt-[245px] mr-5 justify-self-end'
+        className='pt-[245px] mr-5 self-end'
         width={88}
         height={88}
         src={'/assets/chapeuHome.png'}
