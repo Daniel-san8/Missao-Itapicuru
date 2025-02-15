f11df5511e561a287d412ff1d6539cf0e3a5edbd
Daniel San <danielgdt88@hotmail.com>
2025-02-15 16:39:55 -0300

    chore: adiciono swiper ao package

--- src/components/display/Desktop/Home/ArticleHomeDesktop.tsx
+++ src/components/display/Desktop/Home/ArticleHomeDesktop.tsx
@@ -1,11 +1,11 @@
 export default function ArticleHomeDesktop() {
-    return <article className="bg-brand-brown text-white flex flex-col gap-y-6 border border-brand-light rounded-b-[40px] rounded-tr-[40px] w-[1000px] h-[270px] absolute top-[580px] left-44">
-        <h1 className="font-poppins font-bold text-6xl pt-5 ml-10">Ninguém é grande sozinho</h1>
-        <p className="font-medium font-montserrat ml-10 pr-96 text-sm">
-            <span className="font-bold">Ajude-nos</span> a fazer mais por quem precisa! <br />
-            Conheça-nos melhor e <span className="font-bold">contribua do modo que seu coração preferir</span>. <br />
-            “E desta maneira me esforcei para <span className="font-bold">anunciar o evangelho</span>, não onde Cristo haja sido nomeado, para não edificar sobre o fundamento alheio”. <span className="font-bold">(Romanos 15:20)</span>
+    return <article className="lg:bg-brand-brown lg:text-white lg:flex lg:flex-col lg:gap-y-6 lg:border border-brand-light lg:rounded-b-[40px] lg:rounded-tr-[40px] lg:w-[1000px] lg:h-[270px] lg:absolute lg:top-[580px] lg:left-44">
+        <h1 className="lg:font-poppins lg:font-bold lg:text-6xl lg:pt-5 lg:ml-10">Ninguém é grande sozinho</h1>
+        <p className="lg:font-medium lg:font-montserrat lg:ml-10 lg:pr-96 lg:text-sm">
+            <span className="lg:font-bold">Ajude-nos</span> a fazer mais por quem precisa! <br />
+            Conheça-nos melhor e <span className="lg:font-bold">contribua do modo que seu coração preferir</span>. <br />
+            “E desta maneira me esforcei para <span className="lg:font-bold">anunciar o evangelho</span>, não onde Cristo haja sido nomeado, para não edificar sobre o fundamento alheio”. <span className="lg:font-bold">(Romanos 15:20)</span>
         </p>
-        <button className='lg:rounded-[40px] lg:bg-brand-light lg:text-white lg:w-24 lg:h-12 lg:font-dm-sans lg:font-bold lg:text-sm mb-5 ml-10'>DOAR</button>
+        <button className='lg:rounded-[40px] lg:bg-brand-light lg:text-white lg:w-24 lg:h-12 lg:font-dm-sans lg:font-bold lg:text-sm lg:mb-5 ml-10'>DOAR</button>
     </article>
 }
