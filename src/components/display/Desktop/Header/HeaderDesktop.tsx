0515bf98b54f882c8eb2d65b597d07145d9951ff
Daniel San <danielgdt88@hotmail.com>
2025-02-14 23:49:05 -0300

    feat: adiciono lógica e estilo para links do header

--- src/components/display/Desktop/Header/HeaderDesktop.tsx
+++ src/components/display/Desktop/Header/HeaderDesktop.tsx
@@ -3,7 +3,7 @@ import LogoHomeDesktop from "./LogoHomeDesktop";
 import RoutesHeaderDesktop from "./RoutesHeaderDesktop";
 
 export default function HeaderDesktop() {
-    return <header className="lg:flex lg:bg-brand-brown lg:items-center lg:justify-around">
+    return <header className="lg:flex lg:bg-brand-brown lg:items-center lg:justify-around hidden">
         <LogoHomeDesktop />
         <RoutesHeaderDesktop />
         <ButtonHeaderDesktop />
--- src/components/display/Desktop/Header/RoutesHeaderDesktop.tsx
+++ src/components/display/Desktop/Header/RoutesHeaderDesktop.tsx
@@ -1,14 +1,18 @@
 "use client"
+import { usePathname } from "next/navigation"
 import { routesHeader } from "../../../../../public/routesHeader"
 import Link from "next/link"
 
 
 export default function RoutesHeaderDesktop() {
     const routes = routesHeader
-
-    return <ul className="lg:flex lg:text-brand-beige-2 lg:font-bold lg:gap-x-7">
+    const path = usePathname()
+    return <ul className="lg:flex lg:text-brand-beige-2 lg:gap-x-7">
         {routes.map(({ route, name }, index) => {
-            return <li key={index}><Link href={route}>{name}</Link></li>
+            return <li className={`relative ${path === route ? "lg:opacity-100 lg:font-bold" : "lg:opacity-65 lg:font-normal"}`} key={index}>
+                <Link href={route}>{name}</Link>
+                {path === route ? <div className="lg:bg-white lg:h-[2px] lg:z-50 lg:absolute lg:-bottom-full lg:w-full"></div> : null}
+            </li>
         })}
     </ul>
 }
