40ec6c871c0dd1df032d0d5c97c1e832d2a80990
Daniel San <danielgdt88@hotmail.com>
2025-02-15 22:56:08 -0300

    style: atualizo chamada pra components!

--- src/components/Home/ProjectsHome.tsx
+++ src/components/Home/ProjectsHome.tsx
@@ -5,7 +5,7 @@ import SectionCardProjects from './Projects/SectionCardProjects';
 export default function ProjectsHome() {
   return (
     <section className='relative flex flex-col items-center bg-brand-beige-2 md:hidden'>
-      <div className='bg-criancaHome bg-cover w-[327px] h-[470px] mb-12 font-black font-poppins text-4xl text-white text-start  flex rounded-md textShadow absolute -translate-y-2/4 left-6'>
+      <div className='bg-criancaHome bg-cover w-[327px] h-[470px] mb-12 font-black font-poppins text-4xl text-white text-start  flex rounded-md textShadow absolute -translate-y-2/4 left-20'>
         <span className='self-end pr-[31px] pl-[20px] pb-[10px]'>
           Mudamos para sempre o jeito de ver o mundo.
         </span>
--- src/components/display/Desktop/Home/NossosProjetos/CardCarroselProjetosDesktop.tsx
+++ src/components/display/Desktop/Home/NossosProjetos/CardCarroselProjetosDesktop.tsx
@@ -7,13 +7,13 @@ interface ICarrosselProjetos {
 }
 
 export default function CardCarrosselProjetosDesktop({ title, description }: ICarrosselProjetos) {
-    return <div className="bg-white w-80 h-64 rounded-[30px] text-center">
-        <div className="pt-8">
-            <CheckedIcon className="ml-5 bg-brand-light rounded-full p-[2px]" />
+    return <div className="lg:bg-white lg:w-80 lg:h-64 lg:rounded-[30px] lg:text-center">
+        <div className="lg:pt-8">
+            <CheckedIcon className="lg:ml-5 lg:bg-brand-light lg:rounded-full lg:p-[2px]" />
         </div>
-        <div className="mt-32 font-poppins text-base">
-            <p className="font-bold">{title}</p>
-            <p className="font-medium">({description})</p>
+        <div className="lg:mt-32 lg:font-poppins lg:text-base">
+            <p className="lg:font-bold">{title}</p>
+            <p className="lg:font-medium">({description})</p>
         </div>
     </div>
 }
