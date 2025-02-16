eba13c70b05f7dc383ea531511b78b2b90d8420f
Daniel San <danielgdt88@hotmail.com>
2025-02-16 16:49:54 -0300

    feat: componentizo sections da Sobre Nós

--- src/components/display/Desktop/Home/SobreNos/SobreNosDesktop.tsx
+++ src/components/display/Desktop/Home/SobreNos/SobreNosDesktop.tsx
@@ -1,10 +1,7 @@
-import Image from "next/image"
+import OneSectionSobreNosDesktop from "./OneSectionSobreNosDesktop";
 
 export default function SobreNosDesktop() {
-    return <div className="bg-white grid grid-cols-2 px-24 pt-24">
-        <div className="relative">
-            <Image src={"/assets/cardOneSobreNos.png"} width={513} height={508} alt="cardOne" />
-            <p className="absolute font-poppins font-black text-7xl text-white bottom-28 left-7">MISSÃO, <br /> VALOR E <br /> VISÃO</p>
-        </div>
-    </div>
+    return <section>
+        <OneSectionSobreNosDesktop />
+    </section>
 }
